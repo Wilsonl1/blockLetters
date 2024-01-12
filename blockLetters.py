08d4c838b7aff2f1af05c1b00fb9403b33e99dce
Lori Wilson <W.lori03@gmail.com>
2024-01-12 02:19:31 -0500

    first commit

---
+++ blockLetters.py
@@ -0,0 +1,65 @@
+# Lori Wilson
+# My favorite color is red.
+
+# The initials of my name in Block letters and on the same line
+print("L     W   W")
+print("L     W   W")  
+print("L     W   W") 
+print("L     W W W")
+print("L     WW WW") 
+print("L     WW WW")
+print("LLLLL W   W")    
+
+print(
+  # creating space between block letters (don't know if this is the right way or how to do this correctly just yet)
+)
+
+# The initial of my first name in Block letter
+print("L")   
+print("L")  
+print("L") 
+print("L")
+print("L") 
+print("L")
+print("LLLLL")   
+
+print(
+    # creating space between block letters (don't know if this is the right way or how to do this correctly just yet)
+)
+
+# The initial of my last name in Block letter
+print("W   W")
+print("W   W")  
+print("W   W") 
+print("W W W")
+print("WW WW") 
+print("WW WW")
+print("W   W")    
+
+print(
+    # creating space between block letters (don't know if this is the right way or how to do this correctly just yet)
+)
+
+print("I didn't make this, but I thought it was cool and that it would spark some ideas for creativity.")
+
+print(
+    # creating space between block letters (don't know if this is the right way or how to do this correctly just yet)
+)
+
+print("      *     *                    ")
+print("                      *          ")
+print("*         *                 *    ")
+print("     *           *     *         ")
+print("           HHHHHHH        *      ")
+print(" *         HHHHHHH               ")
+print("     *   HHHHHHHHHHH    *     *  ")
+print("         *   @ @   *             ")
+print("        *     ^     *            ")
+print("  *       *       *         *    ")
+print("         SSS * * S               ")
+print("         * SSSSSSSS *            ")
+print("       *      O   SS  *          ")
+print("      *       O    S   *         ")
+print("      *       O        *         ")
+print("       *              *          ")
+print("           *   *   *             ")
